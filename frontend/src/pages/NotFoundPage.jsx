21526ef295fe111e0e23f8f3525b5011bee247c4
Adistiaa <ampunyasepuhakugakjago@gmail.com>
2025-03-03 07:47:35 +0700

    first commit

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -5,22 +5,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const NotFoundPage = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
-      {/* Gambar GIF */}
       <img
-        src="/assets/404.gif" // Ganti ini dengan URL atau path lokal GIF
+        src="https://res.cloudinary.com/dxbkwpm3i/image/upload/v1740962704/error_404_oo6xtc.gif"
         alt="Not Found"
         className="img-fluid mb-4"
         style={{ maxWidth: "300px" }}
       />
 
-      {/* Pesan Error */}
-      <h1 className="fw-bold text-danger">404: NOT_FOUND</h1>
-      <p className="text-muted">Code: <code>NOT_FOUND</code></p>
-      <p className="text-muted">The page you are looking for does not exist.</p>
+      <h1 className="fw-bold">404: NOT_FOUND</h1>
+      <p className="text-muted">Code: <b>NOT_FOUND</b></p>
+      <p className="text-muted">Yah, Halaman nya gak ada nich</p>
 
-      {/* Tombol Kembali */}
       <Link to="/" className="btn btn-primary mt-3">
-        Go Back Home
+        Kembali ke Home Page
       </Link>
     </div>
   );
